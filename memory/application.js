7e15083c076a3b313e96e92b1edcb40d5073e82f
AlexanderRosa <arosa@kippnyccp.org>
2015-03-11 09:22:17 -0400

    Create application.js

---
+++ memory/application.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  	var array=["1","1","2","2","3","3","4","4","5","5","6","6","7","7","8","8","9","9","10","10"]
+  for(i=0; i<20; i++){
+    $('#card_holder').append('<div class="card"><p>'+i+'</p></div>');
+  }
+});
