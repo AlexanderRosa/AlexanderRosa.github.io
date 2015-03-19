d1d542e4481b45ef00dfaad4863a1b7fc9d63b3b
AlexanderRosa <arosa@kippnyccp.org>
2015-03-19 15:50:49 -0400

    Update application.js

--- shopping-list/application.js
+++ shopping-list/application.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
 $('button').on("click", function(){
   $('input').val();
-  var value=$(input').val();
+  var value=$('input').val();
  $('#1').append("<p>"+value+"</p>"); 
      });
- }
- )
+})
