1d106594bca13110f4c03f0ee678498eeb564584
AlexanderRosa <arosa@kippnyccp.org>
2015-05-12 08:57:11 -0400

    Update election.py

--- election.py
+++ election.py
@@ -1,1 +1,15 @@
+from random import radint
+candidate_1=0
 
+for 
+regions = 0
+if randint(1,100) <= 87;#picks percentage and then checks if they won the region
+  regions += 1
+if randint (1,100) <=65;
+  regions += 1
+if randint (1,100) <=17
+  regions +=1
+if region >= 2 
+  candidate_1 += 1
+  
+    print candidate_1 wins
