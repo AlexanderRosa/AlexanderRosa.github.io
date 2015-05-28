cff322c38e9203cf90d7deea6d63307f5d2bca91
AlexanderRosa <arosa@kippnyccp.org>
2015-05-28 08:50:59 -0400

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,8 +1,21 @@
 us_dist(pin)
 from gopigo import
+def turn_right():
+  enc_tgt(0,1,2)
+  time.sleep(0.1)
+  fwd()
+  time.sleep(5)
+  def turn_left
+    enc_tgt(0,1,2)
+    time.sleep(0.1)
+    fwd()
+    time.sleep(5)
 print us_dist(15)
-return distance(cm)
-if us_dist(pin) >= 200;
-move_forward()
-if us_dist(pin) >=50;
-turn right
+for i in range(1,10) :
+  while us_dist(15) >= 20;
+    fwd()
+  random(1,2)
+if 1:
+  turn_right()
+else:
+  turn_lefft()
